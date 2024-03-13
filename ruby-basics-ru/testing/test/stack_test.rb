56e4db8030e1c71d70017ae09d52c0f0cadd874d
@hexlet/cli <support@hexlet.io>
2024-03-13 08:29:17 +0000

    submit ruby-basics-ru/testing

---
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative '../lib/stack'
+
+class StackTest < Minitest::Test
+  # BEGIN
+  def setup
+    @stack = Stack.new %w[first second third fourth]
+  end
+
+  def test_output_should_be_array
+    assert @stack.to_a.class == Array
+  end
+
+  def test_size_of_stack
+    assert @stack.size == 4
+    @stack.push! 'test'
+    assert @stack.size == 5
+    @stack.pop!
+    assert @stack.size == 4
+  end
+
+  def test_push_method
+    @stack.push! 'added'
+    assert @stack.size == 5
+    assert @stack.to_a.last == 'added'
+  end
+
+  def test_pop_method
+    *, last_before = @stack.to_a
+    last = @stack.pop!
+    assert @stack.size == 3
+    assert @stack.to_a.last == 'third'
+    assert last == last_before
+  end
+
+  def test_clear_method
+    @stack.clear!
+    assert @stack.size.zero?
+    assert @stack.to_a.empty?
+  end
+
+  def test_empty_predicate
+    @stack.clear!
+    assert @stack.empty?
+    assert @stack.to_a.empty?
+  end
+  # END
+end
+
+test_methods = StackTest.new({}).methods.select { |method| method.start_with? 'test_' }
+raise 'StackTest has not tests!' if test_methods.empty?
