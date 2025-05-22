d9b3e75b6d5354f45e1cd9ea0ceba17f1bae9c2d
@hexlet/cli <support@hexlet.io>
2025-05-22 11:53:14 +0300

    submit rails-real-ru/search-forms

--- rails-real-ru/search-forms/db/schema.rb
+++ rails-real-ru/search-forms/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2022_09_19_161735) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body"
-    t.string "aasm_state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_220_919_161_735) do
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title', null: false
+    t.text 'body'
+    t.string 'aasm_state'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 end
